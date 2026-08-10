6d648bf71d454091ded71214bf0413f289cf28fd
faysal <nasietfisol2@gmail.com>
2026-08-10 16:55:32 +0300

    this commit will delete all old file

--- src/main/kotlin/spring_boot_backend_notes/database/repository/NoteRepository.kt
+++ src/main/kotlin/spring_boot_backend_notes/database/repository/NoteRepository.kt
@@ -1,13 +1,5 @@
 package spring_boot_backend_notes.database.repository
 
-//import org.bson.types.ObjectId
-//import org.springframework.data.mongodb.repository.MongoRepository
-//import spring_boot_learn.database.model.Note
-//
-//interface NoteRepository:MongoRepository<Note,ObjectId> {
-//    fun findByOwnerId(ownerId: ObjectId): List<Note>
-//}
-
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import spring_boot_backend_notes.database.model.Note
--- src/main/kotlin/spring_boot_backend_notes/database/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/spring_boot_backend_notes/database/repository/RefreshTokenRepository.kt
@@ -1,14 +1,5 @@
 package spring_boot_backend_notes.database.repository
 
-//import org.bson.types.ObjectId
-//import org.springframework.data.mongodb.repository.MongoRepository
-//import spring_boot_learn.database.model.RefreshToken
-//
-//interface RefreshTokenRepository: MongoRepository<RefreshToken, ObjectId> {
-//    fun findByUserIdAndHashedToken(userId: ObjectId, hashedToken: String): RefreshToken?
-//    fun deleteByUserIdAndHashedToken(userId: ObjectId, hashedToken: String): RefreshToken?
-//}
-
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
