d64042ec77d9b0d83274fa9e03387ab11a7e13ee
faysal <nasietfisol2@gmail.com>
2026-08-12 09:22:00 +0300

    This commit modified user.kt and application.properties

--- src/main/kotlin/spring_boot_backend_notes/database/model/User.kt
+++ src/main/kotlin/spring_boot_backend_notes/database/model/User.kt
@@ -1,16 +1,5 @@
 package spring_boot_backend_notes.database.model
 
-//import org.bson.types.ObjectId
-//import org.springframework.data.annotation.Id
-//import org.springframework.data.mongodb.core.mapping.Document
-//
-//@Document("users")
-//data class User(
-//    val email: String,
-//    val hashedPassword: String,
-//    @Id val id: ObjectId = ObjectId()
-//)
-
 import jakarta.persistence.*
 
 @Entity
